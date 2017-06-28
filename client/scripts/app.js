8ee6749a8f93e4b442458f2394528faa9aaa1861
Jessica D'Andrea <jessica.dandrea@pwc.com>
2017-06-27 22:07:33 -0500

    Completed bare minimum requirements

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,7 +5,10 @@ var Movie = Backbone.Model.extend({
   },
 
   toggleLike: function() {
-    // your code here
+
+    this.set('like', !this.get('like'));
+
+
   }
 
 });
@@ -15,13 +18,15 @@ var Movies = Backbone.Collection.extend({
   model: Movie,
 
   initialize: function() {
-    // your code here
+    this.on('change', this.sort);
   },
 
   comparator: 'title',
 
   sortByField: function(field) {
-    // your code here
+
+    this.comparator = field;
+    this.sort();
   }
 
 });
@@ -58,7 +63,7 @@ var MovieView = Backbone.View.extend({
                         </div>'),
 
   initialize: function() {
-    // your code here
+    this.on('change', this.render());
   },
 
   events: {
@@ -66,7 +71,7 @@ var MovieView = Backbone.View.extend({
   },
 
   handleClick: function() {
-    // your code here
+    this.model.toggleLike();
   },
 
   render: function() {
@@ -79,7 +84,7 @@ var MovieView = Backbone.View.extend({
 var MoviesView = Backbone.View.extend({
 
   initialize: function() {
-    // your code here
+    this.on('change', this.render());
   },
 
   render: function() {
